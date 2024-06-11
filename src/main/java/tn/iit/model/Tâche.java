20541d4acce6fa11722eefeb90f4b8fd40a3e256
Ons Hadrich <ounshadriche@gmail.com>
2024-06-11 21:20:41 +0300

    Set up model tache

--- src/main/java/tn/iit/model/Tâche.java
+++ src/main/java/tn/iit/model/Tâche.java
@@ -1,24 +1,24 @@
 package tn.iit.model;
 
 public class Tâche {
-	private Teacher enseignant;
+	private String nom_enseignant;
 	private int nombreCopies;
 	private String dateReception;
 	private String documentImprimer;
 
-	public Tâche(Teacher enseignant, int nombreCopies, String dateReception, String documentImprimer) {
-		this.enseignant = enseignant;
+	public Tâche(String nom_enseignant, int nombreCopies, String dateReception, String documentImprimer) {
+		this.nom_enseignant = nom_enseignant;
 		this.nombreCopies = nombreCopies;
 		this.dateReception = dateReception;
 		this.documentImprimer = documentImprimer;
 	}
 
 	public String getNomEnseignant() {
-		return enseignant.getUsername();
+		return nom_enseignant;
 	}
 
 	public void setNomEnseignant(String nomEnseignant) {
-		this.enseignant.setUsername(nomEnseignant);
+		this.nom_enseignant= nomEnseignant;
 	}
 
 	public int getNombreCopies() {
